c7f637951eb0c22b978cad5a1f0d74dda4c22878
r0s0dgv <ritesh.singh1@walmart.com>
2022-04-03 00:02:37 +0530

    Code cleanup added .gitignore

--- src/main/java/singh/ritesh/dream11combinations/bo/Franchise.java
+++ src/main/java/singh/ritesh/dream11combinations/bo/Franchise.java
@@ -2,12 +2,20 @@ package singh.ritesh.dream11combinations.bo;
 
 public enum Franchise
 {
-    MI,
-    DC,
-    RCB,
-    KKR,
-    SRH,
-    CSK,
-    RR,
-    KXIP;
+    MI("Mumbai Indians"),
+    DC("Delhi Capitals"),
+    RCB("Royal Challengers Bangalore"),
+    KKR("Kolkata knight riders"),
+    SRH("SunRisers Hyderabad"),
+    CSK("Chennai Super kings"),
+    RR("Rajasthan Royals"),
+    PK("Punjab Kings"),
+    GT("Gujarat Titans"),
+    LSG("Lucknow Super Giants");
+
+    private String name;
+
+    Franchise(String s) {
+        this.name = s;
+    }
 }
